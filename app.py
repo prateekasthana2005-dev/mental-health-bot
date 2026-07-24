ac1fbc012d46b0edd752af4588ecc9219aa07f7f
prateekasthana2005-dev <prateek.asthana2005@gmail.com>
2026-07-24 14:40:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ if not api_key:
 
 # Configure GenAI
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-3.5-flash")
 
 # Session Chat History
 if "messages" not in st.session_state:
